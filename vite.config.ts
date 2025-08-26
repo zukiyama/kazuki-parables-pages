dea2f0fed3f7cbe2fd04660671efd2fec65d8b28
zukiyama <jon_tinygiant@hotmail.com>
2025-08-26 16:21:00 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/kazuki-parables-pages-main/' : '/',
+  base: mode === 'production' ? '/kazuki-parables-pages/' : '/',
   build: {
     outDir: mode === 'production' ? '.' : 'dist',
     emptyOutDir: false,
